66961db169367eb99a2f1190589708ee8367959b
hadrien le bret <hadrien.lebret@gmail.com>
2020-11-24 18:51:51 +0100

    show

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,14 @@
 class EventsController < ApplicationController
+  before_action :find_event, only: [:show]
+  skip_before_action :authenticate_user!, only: [:show]
+
+  def show
+    authorize @event
+  end
+
+  private
+
+  def find_event
+    @event = Event.find(params[:id])
+  end
 end

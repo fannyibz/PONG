b506704d3c13f65ae7c8b0dc0674fed5bd365c56
raphael allard <raphael.s.allard@gmail.com>
2020-11-24 13:22:42 +0100

    validation + bugs fixed has-many + dependent

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :event_users, dependent: :destroy
   has_many :events, through: :event_users
-  has_many :messages
+  has_many :messages, dependent: :destroy
+  # has_one_attached :avatar => à décommenter quand cloudinary est configuré
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end

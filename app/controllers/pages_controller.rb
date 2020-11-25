71f3519d9a2a43f284e935dac66fc1955511f987
raphael allard <raphael.s.allard@gmail.com>
2020-11-25 10:54:07 +0100

    bug fixed home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,8 @@
 class PagesController < ApplicationController
-
   def home
       @hosted_events = policy_scope(current_user.events)
       @invited_events = policy_scope(current_user.events_as_guest)
       authorize @hosted_events if @hosted_events.empty?
       authorize @invited_events if @invited_events.empty?
   end
-
 end

9526cb24b471324749dd7622c5118319daa2df8a
hadrien le bret <hadrien.lebret@gmail.com>
2020-11-25 15:14:16 +0100

    search bar in mapbox

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,6 +1,5 @@
-// Mapbox, initiate
-
-import mapboxgl from 'mapbox-gl';
+import mapboxgl from 'mapbox-gl'; // Mapbox, initiate
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder'; // Mapbox, searching on your map
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
@@ -9,7 +8,7 @@ const initMapbox = () => {
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
   const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10'
+    style: 'mapbox://styles/hahahadrien/ckhvtwp2f083919p5pg8jfyxa' // <-- use your own to styling your map
   });
 
   const markers = JSON.parse(mapElement.dataset.markers); // Mapbox, add markers
@@ -18,6 +17,10 @@ const initMapbox = () => {
       .setLngLat([ marker.lng, marker.lat ])
       .addTo(map);
   });
+
+  // Mapbox, searching on your map
+  map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+                                      mapboxgl: mapboxgl }));
 
   fitMapToMarkers(map,markers)
 };
@@ -36,30 +39,21 @@ const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
     const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
 
-    new mapboxgl.Marker()
-      .setLngLat([ marker.lng, marker.lat ])
-      .setPopup(popup) // add this
+    // Custom markers
+    // 1-Create a HTML element for your custom marker
+    const element = document.createElement('div');
+    element.className = 'marker';
+    element.style.backgroundImage = `url('${marker.image_url}')`;
+    element.style.backgroundSize = 'contain';
+    element.style.width = '25px';
+    element.style.height = '25px';
+
+    // 2-Pass the element as an argument to the new marker
+    new mapboxgl.Marker(element)
+      .setLngLat([marker.lng, marker.lat])
+      .setPopup(popup)
       .addTo(map);
   });
 };
-
-// Custom markers
-markers.forEach((marker) => {
-  const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
-
-  // Create a HTML element for your custom marker
-  const element = document.createElement('div');
-  element.className = 'marker';
-  element.style.backgroundImage = `url('${marker.image_url}')`;
-  element.style.backgroundSize = 'contain';
-  element.style.width = '25px';
-  element.style.height = '25px';
-
-  // Pass the element as an argument to the new marker
-  new mapboxgl.Marker(element)
-    .setLngLat([marker.lng, marker.lat])
-    .setPopup(popup)
-    .addTo(map);
-});
 
 export { initMapbox };

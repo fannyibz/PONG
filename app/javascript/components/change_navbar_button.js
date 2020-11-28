37483140fa3b4cb76f29f65d70079e22b7dba9db
raphael allard <raphael.s.allard@gmail.com>
2020-11-28 17:52:45 +0100

    function js still to improve navbar

--- app/javascript/components/change_navbar_button.js
+++ app/javascript/components/change_navbar_button.js
@@ -1,11 +1,13 @@
 const changeNavbarButton = () => {
 
-  const navbarBoxActive = document.querySelector(".navbar-icon-box-active")
   const navbarBox = document.querySelectorAll(".navbar-icon-box");
-
+  
   navbarBox.forEach((box) => {
     box.addEventListener("click", (event) => {
-      navbarBoxActive.classList.remove("navbar-icon-box-active")
+
+      const navbarBoxActive = document.querySelector(".navbar-icon-box-active");
+      
+      navbarBoxActive.classList.remove("navbar-icon-box-active");
       event.currentTarget.classList.add("navbar-icon-box-active");
       console.log(event.currentTarget);
     });

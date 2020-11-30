d58d9481204090d3c9c74c8cea18ee7de78f31e6
fannyibanez <fannyibanez89@gmail.com>
2020-11-30 10:19:14 +0100

    updzte pwa

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  config.hosts << "c878e5fdb3b7.ngrok.io"
+
   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
   # Settings specified here will take precedence over those in config/application.rb.
 

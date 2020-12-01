c5a6d5bd52bbd5f6957e73ae6a10e2ef462b5949
raphael allard <raphael.s.allard@gmail.com>
2020-12-01 16:12:39 +0100

    added bubbles behavior to chat

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,10 +9,11 @@ class MessagesController < ApplicationController
     if @message.save
       ChatroomChannel.broadcast_to(
         @event,
-        render_to_string(partial: "message", locals: { message: @message })
+        message: render_to_string(partial: "message", locals: { message: @message, is_author: false }),
+        author_id: current_user.id
       )
 
-      redirect_to chatroom_event_path(@event, anchor: "message-#{@message.id}")
+      # redirect_to chatroom_event_path(@event, anchor: "message-#{@message.id}") --> comportement directement initié dans notre function JS chatroom
     else
       render "events/chatroom"
     end
--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -7,9 +7,33 @@ const initChatroomCable = () => {
 
     consumer.subscriptions.create({ channel: "ChatroomChannel", id: id }, {
       received(data) {
-        messagesContainer.insertAdjacentHTML('beforeend', data);
+        messagesContainer.insertAdjacentHTML('beforeend', data.message);
+        scrollLastMessageIntoView();
+        addAuthorClassToLastMessage(data);
       },
     });
+  }
+}
+
+const scrollLastMessageIntoView = () => {
+  const messages = document.querySelectorAll('.message-container');
+  const lastMessage = messages[messages.length - 1];
+  if (lastMessage !== undefined) {
+    lastMessage.scrollIntoView();
+  }
+}
+
+const addAuthorClassToLastMessage = (data) => {
+  const messages = document.querySelectorAll('.message-container');
+  const lastMessage = messages[messages.length - 1];
+  const userId = document.getElementById('messages').dataset.userId;
+  const form = document.getElementById('new_message');
+
+  if (lastMessage !== undefined) {
+    if (data.author_id == userId) {
+      lastMessage.classList.add("is-author");
+      form.reset();
+    }
   }
 }
 

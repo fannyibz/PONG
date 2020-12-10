f43170b07e1f0bf2550adb0e4a7f39e0ba8f57ed
Tristan Simon <tristan.simon1@outlook.fr>
2020-12-10 19:00:38 +0100

    fix bug facebook

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
 
   include Pundit
   after_action :verify_authorized, except: [:index, :home], unless: :skip_pundit?
-  after_action :verify_policy_scoped, only: [:index, :home], unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: [:index], unless: :skip_pundit?
+
 
   private
 
@@ -13,5 +14,11 @@ class ApplicationController < ActionController::Base
 
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
 end

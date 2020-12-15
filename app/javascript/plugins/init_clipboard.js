07fa9307a6529b5e30e35b3923816c52c90d6682
Magali Rousseau <rousseaumagali@gmail.com>
2020-12-15 09:58:35 +0100

    link working on the modal window

--- app/javascript/plugins/init_clipboard.js
+++ app/javascript/plugins/init_clipboard.js
@@ -1,29 +1,31 @@
 const initClipboard = () => {
   const buttonShareLink = document.getElementById("share-link");
-  
+
   if (buttonShareLink) {
     buttonShareLink.addEventListener("click", () => {
       const url = document.querySelector("meta[property='og:url']").getAttribute("content");
       navigator.clipboard.writeText(url);
     });
   }
-  
-  const markerAvatar = document.querySelector(".marker");
-  
+
+  const markerAvatar = document.querySelectorAll(".marker");
+
   if (markerAvatar) {
-    markerAvatar.addEventListener("click", () => {
-      setTimeout(function () {
-        const idShowModal = document.getElementById("url-show-hidden").innerText;
-        console.log(idShowModal);
-        const buttonShareLinkModal = document.getElementById("share-link-modal");
-  
-        buttonShareLinkModal.addEventListener("click", () => {
-          const url = `${document.querySelector("meta[property='og:url']").getAttribute("content")}events/${idShowModal}`;
-          navigator.clipboard.writeText(url);
-          });
-      }, 100);
+    markerAvatar.forEach((marker) => {
+      marker.addEventListener("click", () => {
+        setTimeout(function () {
+          const idShowModal = document.getElementById("url-show-hidden").innerText;
+          console.log(idShowModal);
+          const buttonShareLinkModal = document.getElementById("share-link-modal");
+
+          buttonShareLinkModal.addEventListener("click", () => {
+            const url = `${document.querySelector("meta[property='og:url']").getAttribute("content")}events/${idShowModal}`;
+            navigator.clipboard.writeText(url);
+            });
+        }, 100);
+      });
     });
   }
 };
-  
+
 export { initClipboard };

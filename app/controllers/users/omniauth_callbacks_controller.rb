4d0967fa285eab721e6ecff3840eb928399045a0
hadrien le bret <hadrien.lebret@gmail.com>
2020-12-19 13:49:07 +0100

    ajout de qlq lignes de codes omniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   skip_before_action :verify_authenticity_token, only: :facebook
-  
+
   def facebook
     user = User.find_for_facebook_oauth(request.env['omniauth.auth'])
 
@@ -12,7 +12,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     else
       session['devise.facebook_data'] = request.env['omniauth.auth']
       redirect_to new_user_registration_url
-      end
+    end
+  end
+
+  def failure
+    redirect_to root_path
   end
 
 end
